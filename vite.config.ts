27b6ab8190bcc57a588e0b8a43da3ce0b30a1ef6
vetan2 <vetan2@naver.com>
2022-09-18 11:01:25 +0900

    Feature/storybook (#33)
    * Install storybook
    
    * Resolve errors & Build storybook
    
    * Add storybook build path to gitignore
    
    * Add gh page action

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 import visualizer from "rollup-plugin-visualizer";
 import inject from "@rollup/plugin-inject";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd());
 
   const envWithProcessPrefix = Object.entries(env).reduce(
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
   );
 
   return {
-    plugins: [viteReact(), envCompatible({ prefix: "REACT_APP_" })],
+    plugins: [viteReact(), envCompatible({prefix: "REACT_APP_"})],
     build: {
       outDir: "build",
       rollupOptions: {
@@ -40,6 +40,38 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         "~": path.resolve(__dirname, "./src"),
+        util: 'rollup-plugin-node-polyfills/polyfills/util',
+        sys: 'util',
+        events: 'rollup-plugin-node-polyfills/polyfills/events',
+        stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+        path: 'rollup-plugin-node-polyfills/polyfills/path',
+        querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
+        punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+        url: 'rollup-plugin-node-polyfills/polyfills/url',
+        string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder',
+        http: 'rollup-plugin-node-polyfills/polyfills/http',
+        https: 'rollup-plugin-node-polyfills/polyfills/http',
+        os: 'rollup-plugin-node-polyfills/polyfills/os',
+        assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+        constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+        _stream_duplex:
+          'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+        _stream_passthrough:
+          'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+        _stream_readable:
+          'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+        _stream_writable:
+          'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+        _stream_transform:
+          'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+        timers: 'rollup-plugin-node-polyfills/polyfills/timers',
+        console: 'rollup-plugin-node-polyfills/polyfills/console',
+        vm: 'rollup-plugin-node-polyfills/polyfills/vm',
+        zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+        tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+        domain: 'rollup-plugin-node-polyfills/polyfills/domain',
+        buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+        process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
       },
     },
   };
